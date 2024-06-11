87bc8b86286d7f1ac5fde196648679abcead8713
aempantaleao <anthony.medeiros@fazenda.se.gov.br>
2024-06-11 20:42:57 -0300

    Mapstrut conversao entidade para response dto

--- proposta-app/src/main/java/com/proposta/proposta_app/mapper/PropostaMapper.java
+++ proposta-app/src/main/java/com/proposta/proposta_app/mapper/PropostaMapper.java
@@ -1,12 +1,16 @@
 package com.proposta.proposta_app.mapper;
 
 import com.proposta.proposta_app.dto.PropostaRequestDto;
+import com.proposta.proposta_app.dto.PropostaResponseDto;
 import com.proposta.proposta_app.entity.Proposta;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface PropostaMapper {
+
+    PropostaMapper INSTANCE = Mappers.getMapper(PropostaMapper.class);
 
     @Mapping(target = "usuario.nome", source = "nome")
     @Mapping(target = "usuario.sobrenome", source = "sobrenome")
@@ -17,5 +21,14 @@ public interface PropostaMapper {
     @Mapping(target = "aprovado", ignore = true)
     @Mapping(target = "integrada", ignore = true)
     Proposta convertDtoToProposta(PropostaRequestDto propostaRequestDto);
+
+
+    @Mapping(target = "nome", source = "usuario.nome")
+    @Mapping(target = "sobrenome", source = "usuario.sobrenome")
+    @Mapping(target = "cpf", source = "usuario.cpf")
+    @Mapping(target = "telefone", source = "usuario.telefone")
+    @Mapping(target = "renda", source = "usuario.renda")
+    @Mapping(target = "observacao",ignore = true)
+    PropostaResponseDto convertEntityToDto(Proposta proposta);
 
 }
