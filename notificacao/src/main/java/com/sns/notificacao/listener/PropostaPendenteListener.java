206b3e108bc6deadb9b829de37fd8a5174df89c3
anthonyMeds <anthony.eduardo@hotmail.com>
2024-06-18 12:09:12 -0300

    NotificacaoSnsService e mensagem constante

--- notificacao/src/main/java/com/sns/notificacao/listener/PropostaPendenteListener.java
+++ notificacao/src/main/java/com/sns/notificacao/listener/PropostaPendenteListener.java
@@ -1,13 +1,23 @@
 package com.sns.notificacao.listener;
 
+import com.sns.notificacao.domain.Proposta;
+import com.sns.notificacao.service.NotificacaoSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import static com.sns.notificacao.constante.MensagemConstante.PROPOSTA_EM_ANALISE;
 
 @Component
 public class PropostaPendenteListener {
 
+    private NotificacaoSnsService notificacaoSnsService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-    public void propostaPendente() {
+    public void propostaPendente(Proposta proposta) {
+
+        String mensagem = String.format(PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+
+        notificacaoSnsService.notificar(mensagem);
 
     }
 }
