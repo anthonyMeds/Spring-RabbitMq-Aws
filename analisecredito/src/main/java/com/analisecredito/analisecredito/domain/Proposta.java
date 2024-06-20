761341ad880556eff1bd714e2b0c7a3bef00dea0
anthonyMeds <anthony.eduardo@hotmail.com>
2024-06-20 14:42:30 -0300

    setup MS analise de credito

---
+++ analisecredito/src/main/java/com/analisecredito/analisecredito/domain/Proposta.java
@@ -0,0 +1,26 @@
+package com.analisecredito.analisecredito.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Proposta {
+
+    private Long id;
+
+    private Double valorSolicitado;
+
+    private int prazoPagamento;
+
+    private Boolean aprovado;
+
+    private boolean integrada;
+
+    private String observacao;
+
+    private Usuario usuario;
+
+
+
+}
