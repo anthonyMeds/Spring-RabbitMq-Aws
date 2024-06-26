78d93bdfcdef5139a23f8ead91f2d5dad7a27757
anthonyMeds <anthony.eduardo@hotmail.com>
2024-06-26 20:00:54 -0300

    feat : salvar as propostas que vem da fila de concluidas - wip

---
+++ proposta-app/src/main/java/com/proposta/proposta_app/listener/PropostaConcluidaListener.java
@@ -0,0 +1,20 @@
+package com.proposta.proposta_app.listener;
+
+import com.proposta.proposta_app.entity.Proposta;
+import com.proposta.proposta_app.repository.PropostaRepository;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropostaConcluidaListener {
+
+    @Autowired
+    private PropostaRepository propostaRepository;
+
+    @RabbitListener(queues = "rabbitmq.propostaconcluida.exchange")
+    public void cadastrar(Proposta proposta) {
+        propostaRepository.save(proposta);
+    }
+
+}
