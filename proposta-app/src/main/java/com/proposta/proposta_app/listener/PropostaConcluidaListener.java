e0e61806ca4370ce53fc967f141862dedfac48ca
anthonyMeds <anthony.eduardo@hotmail.com>
2024-06-26 21:02:43 -0300

    feat : implementação do web socket

--- proposta-app/src/main/java/com/proposta/proposta_app/listener/PropostaConcluidaListener.java
+++ proposta-app/src/main/java/com/proposta/proposta_app/listener/PropostaConcluidaListener.java
@@ -1,7 +1,10 @@
 package com.proposta.proposta_app.listener;
 
+import com.proposta.proposta_app.dto.PropostaResponseDto;
 import com.proposta.proposta_app.entity.Proposta;
+import com.proposta.proposta_app.mapper.PropostaMapper;
 import com.proposta.proposta_app.repository.PropostaRepository;
+import com.proposta.proposta_app.service.WebSocketService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,8 +15,15 @@ public class PropostaConcluidaListener {
     @Autowired
     private PropostaRepository propostaRepository;
 
+    @Autowired
+    private WebSocketService webSocketService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposta.concluida}")
     public void propostaConcluida(Proposta proposta) {
         propostaRepository.save(proposta);
+
+        PropostaResponseDto propostaResponseDto = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+
+        webSocketService.notificar(propostaResponseDto);
     }
 }
---
+++ proposta-app/src/main/java/com/proposta/proposta_app/service/WebSocketService.java
@@ -0,0 +1,18 @@
+package com.proposta.proposta_app.service;
+
+import com.proposta.proposta_app.dto.PropostaResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebSocketService {
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    public void notificar(PropostaResponseDto proposta) {
+        template.convertAndSend("/propostas", proposta);
+    }
+
+}
